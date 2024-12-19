17fdbf20ed8981828353f6a7947189447743a553
ZuongZuong <binhduong190599@gmail.com>
2024-12-19 21:14:20 +0700

    Variable

---
+++ Lessons/VariableLearning.js
@@ -0,0 +1,14 @@
+// Variable
+// Hoisting
+
+// = assignment operand
+
+// Declaration == Re-declaration is NOT allowed, Re-assign is acceptable
+
+
+let a = 1;
+
+
+// Constant
+
+const PI_NUMBER = 3.14;
