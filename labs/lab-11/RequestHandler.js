56bf0cedecbeba430ca20f5fa83f1e39fddc7549
ZuongZuong <binhduong190599@gmail.com>
2025-03-04 02:23:46 +0700

    Add Lab 11

---
+++ labs/lab-11/RequestHandler.js
@@ -0,0 +1,50 @@
+const { Post } = require("./Post");
+
+class RequestHandler {
+
+    constructor(url) {
+        this._url = url
+    }
+    
+    async getResponse() {
+        const response = await fetch(this._url);
+        return await response.json();
+    }
+
+    async printTargetPost(userId, postId) {
+        // Construct the returned data as a Post data model from class Post
+        let temp
+        const data = await this.getResponse();
+        let posts = await data.filter(function (post) {
+            if (post.userId === userId && post.id === postId) {
+                temp = new Post(post.userId, post.id, post.title, post.body);
+            }
+        });
+        return temp;
+    }
+
+    async printAllPosts(userId) {
+        // Construct the returned data as a [ Post data model ] from class Post
+        let temp = []
+        const data = await this.getResponse();
+        let posts = data.filter(function (post) {
+            if (post.userId === userId) {
+                temp.push(new Post(post.userId, post.id, post.title, post.body));
+            }
+        });
+        return temp;
+    }
+
+    async _getAllPosts(userId) {
+        // Get all posts from the API
+        let posts = []
+        const data = await this.getResponse();
+        for (let post of data) {
+            posts.push(new Post(post.userId, post.id, post.title, post.body));
+        }
+        return posts;
+    }
+
+}
+
+module.exports = RequestHandler;
---
+++ labs/lab-11/TestPostModel.js
@@ -0,0 +1,23 @@
+const Post = require("./Post");
+const RequestHandler = require("./RequestHandler");
+
+// Excution
+lab11();
+
+// Function Declaration
+async function lab11(){
+    // Given params
+    const userId = 1;
+    const postId = 5;
+
+    // Create RequestHandler object
+    const requestHandler = new RequestHandler("https://jsonplaceholder.typicode.com/posts");
+    let printTargetPost = await requestHandler.printTargetPost(userId, postId);
+    console.log(printTargetPost);
+
+    let printAllPosts = await requestHandler.printAllPosts(userId);
+    console.log(printAllPosts);
+
+    let getAllPost = await requestHandler._getAllPosts(userId);
+    console.log(getAllPost);
+}
